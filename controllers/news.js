3d1ecf96945e6f9c5a19c0f6579a52457cac2b0d
Treudey <alextreude@hotmail.com>
2019-06-05 14:39:03 -0400

    got database working with handlebars dsplaying articles

---
+++ controllers/news.js
@@ -0,0 +1,96 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const db = require('../models');
+
+// private helper functions
+const addArticle = (articleObj) => {
+  db.Article.findOne({ headline: articleObj.headline }, (err, article) => {
+         
+    if (err) throw err;
+ 
+    //check if the article was found
+    if (article) {
+      console.log('Article already exists');
+    } else {
+      //code if no user with entered email was found
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(articleObj)
+        .then(dbArticle => {
+          console.log(dbArticle);
+        })
+        .catch(err => {
+          if (err) throw err;
+        });
+    }
+  });
+};
+
+exports.getArticles = (req, res) => {
+  db.Article.find({})
+    .then(dbArticles => {
+      // If any Articles are found, send them to the client
+      const hbsObj = {
+        articles: dbArticles,
+        pageTitle: 'Articles'
+      }
+      
+      res.render('index', hbsObj);
+    })
+    .catch(err =>{
+      // If an error occurs, send it back to the client
+      if (err) throw err;
+    });
+};
+
+exports.scrapeSites = (req, res) => {
+  // First, we grab the bodies of the html with axios
+  axios.get("https://www.sportsnet.ca/").then(response => {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    const $ = cheerio.load(response.data);
+
+    $(".post").each((i, element) => {
+      
+      // Add the text and href of every link, and save them as properties of the result object
+      const link = $(element).find("h3").children().attr("href") || $(element).find("h4").children().attr("href");;
+      const title = $(element).find("h3").children().text() || $(element).find("h4").children().text();
+      const img = $(element).find("img").attr("src");
+
+      if (title && link && img) {
+        // Save an empty result object
+        const result = {};
+        result.url = link;
+        result.headline = title;
+        result.image = img;
+
+        addArticle(result);
+      }
+    });
+  });
+
+  axios.get("https://www.espn.com/").then(response => {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    const $ = cheerio.load(response.data);
+
+    $(".contentItem__content--story").each((i, element) => {
+      
+      // Add the text and href of every link, and save them as properties of the result object
+      const link = $(element).find("a").attr("href");
+      const title = $(element).find("h1").text();
+      const img = $(element).find("img.media-wrapper_image").data("default-src") || $(element).find("img").attr("src");
+
+      if (link && img && title) {
+        // Save an empty result object
+        const result = {};
+        result.url = link.includes("https") ? link : 'https://www.espn.com' + link;
+        result.headline = title;
+        result.image = img;
+
+        addArticle(result);
+      }
+    });
+
+    // Send a message to the client
+    res.send("Scrape Complete");
+  });
+};
--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
-const axios = require('axios');
-const cheerio = require('cheerio');
 
-const db = require('./models');
+const newsController = require('./controllers/news');
 
 const connectionString = 'mongodb+srv://alex:67ECNUeIAsc30u04@cluster0-ofcaa.mongodb.net/scraper?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 3000;
@@ -26,7 +24,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.engine(
   'hbs',
   exphbs({
-    defaultLayout: 'main',
+    defaultLayout: 'main.hbs',
   }),
 );
 app.set('view engine', 'hbs');
@@ -36,107 +34,13 @@ mongoose.connect(connectionString, { useNewUrlParser: true });
 
 // Routes
 
-// A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("https://www.sportsnet.ca/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    const $ = cheerio.load(response.data);
-
-    $(".post").each(function(i, element) {
-      
-      // Add the text and href of every link, and save them as properties of the result object
-      const link = $(this).find("h3").children().attr("href") || $(this).find("h4").children().attr("href");;
-      const title = $(this).find("h3").children().text() || $(this).find("h4").children().text();
-      const img = $(this).find("img").attr("src");
-
-      if (title && link) {
-        // Save an empty result object
-        const result = {};
-        result.url = link;
-        result.headline = title;
-        result.image = img || 'none';
-        console.log(result);
-
-
-        db.Article.findOne({ headline: result.headline }, (err, article) => {
-          if(err) {
-             //handle error here
-          }
-       
-          //if a user was found, that means the user's email matches the entered email
-          if (article) {
-            var err = new Error('A user with that email has already registered. Please use a different email..')
-            err.status = 400;
-            return next(err);
-          } else {
-            //code if no user with entered email was found
-          }
-       }); 
-      }
-      console.log('sportsnet' + i);
-      /*
-
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, log it
-          console.log(err);
-        });
-      */
-    });
-  });
-  axios.get("https://www.espn.com/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    const $ = cheerio.load(response.data);
-
-    $(".contentItem__content--story").each(function(i, element) {
-      
-      // Add the text and href of every link, and save them as properties of the result object
-      const link = $(this).find("a").attr("href");
-      const title = $(this).find("h1").text();
-      const img = $(this).find("img").attr("src");
-
-      if (link) {
-        // Save an empty result object
-        const result = {};
-        result.url = link;
-        result.headline = title;
-        result.image = img || 'none';
-        console.log(result);
-      }
-      console.log('espn' + i);
-      /*
-
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, log it
-          console.log(err);
-        });
-      */
-    });
-
-    // Send a message to the client
-    res.send("Scrape Complete");
-  });
-});
-
 // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
-  // TODO: Finish the route so it grabs all of the articles
-});
+app.get("/", newsController.getArticles);
 
+// A GET route for scraping the sports news websites
+app.get("/scrape", newsController.scrapeSites);
 
 // Start Server
-app.listen(PORT, function() {
+app.listen(PORT, () => {
   console.log('App running on port ' + PORT + '!');
 });
